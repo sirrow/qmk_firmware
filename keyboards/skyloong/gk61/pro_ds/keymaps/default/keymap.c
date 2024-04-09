e8f535f26fe4c868650b08bf0f9e705621b56327
sirrow <sirrow@gmail.com>
2024-04-09 20:24:25 +0900

    compliant to keyboard pikapika system

--- keyboards/skyloong/gk61/pro_ds/keymaps/default/keymap.c
+++ keyboards/skyloong/gk61/pro_ds/keymaps/default/keymap.c
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "raw_hid.h"
+#include "rgb_matrix.h"
 
 enum layer_names {
     _WIN,
@@ -71,3 +73,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+
+enum HID_RGBLED_COMMAND { HID_PING = 0, HID_SET_HSV, HID_SET_RGB };
+void raw_hid_receive(uint8_t *data, uint8_t length) {
+    switch (data[0]) {
+        case HID_PING:
+            data[0] = 1;
+            data[1] = 'R';
+            data[2] = 'G';
+            data[3] = 'B';
+            break;
+        case HID_SET_HSV:
+            rgblight_sethsv_noeeprom(data[1], data[2], data[3]);
+            data[0] = 1;
+            break;
+/*
+        case HID_SET_RGB:
+            rgblight_setrgb(data[1], data[2], data[3]);
+            data[0] = 1;
+            break;
+*/
+        default:
+            data[1] = data[0];
+            data[0] = 0;
+            break;
+    }
+    raw_hid_send(data, length);
+}
+
+void keyboard_post_init_user(void) {
+    // 起動時にRGBLEDを有効化して消灯する
+    rgblight_enable_noeeprom();
+    //rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_sethsv_noeeprom(0,0,0);
+}
